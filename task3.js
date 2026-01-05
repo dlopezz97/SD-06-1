ba54e2c368b32622c818cf0d456f460e867b1e38
dlopez <ulopez.rdz@gmail.com>
2026-01-05 14:28:56 -0600

    tareas 1-4

--- task3.js
+++ task3.js
@@ -3,11 +3,12 @@
 function Mail(subj, msg) {
     this.subject = subj
     this.message = msg
-    
+    this.printMail=()=>{
+    console.log(newMail.subject + ": " + newMail.message)
+    }
   }
-  
-  const newMail = new Mail(  )
-  
+  const newMail = new Mail(process.argv[2],process.argv[3] )
+  newMail.printMail();
   // Type your code above this line!
   
-  newMail.printMail()
+  
